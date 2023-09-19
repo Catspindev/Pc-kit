e9311e4199495d6864e960604ffcb54eae735719
Catspindev <Catspindev@catspin.eu.org>
2023-09-19 16:43:45 +0200

    Update Optimizer.bat

--- Optimizer.bat
+++ Optimizer.bat
@@ -52,34 +52,16 @@ if "%choice%"=="4" (
 if "%choice%"=="5" (
     echo Installing popular tools...
 
-    echo Checking if curl is installed...
-    where curl >nul 2>nul
-    if %errorlevel% neq 0 (
-        echo curl is not installed. Downloading curl installer...
-        curl -o curl_installer.exe https://curl.se/windows/dl-7.78.0_1/curl-7.78.0_1-win64-mingw.zip
-        echo.
-        echo Installing curl...
-        start "" curl_installer.exe
-        echo.
-        echo curl installed successfully.
-    ) else (
-        echo curl is already installed.
-    )
-
-    echo Downloading OBS installer...
-    curl -o obs_installer.exe https://cdn-fastly.obsproject.com/downloads/OBS-Studio-27.0.1-Full-Installer-x64.exe
-    echo.
-    echo Running OBS installer...
+    echo Installing OBS...
+    powershell.exe -Command "Start-BitsTransfer -Source https://cdn-fastly.obsproject.com/downloads/OBS-Studio-27.0.1-Full-Installer-x64.exe -Destination obs_installer.exe"
     start "" obs_installer.exe /S
 
-    echo Downloading 7-Zip installer...
-    curl -o 7zip_installer.exe https://www.7-zip.org/a/7z1900-x64.exe
-    echo.
-    echo Running 7-Zip installer...
+    echo Installing 7-Zip...
+    powershell.exe -Command "Start-BitsTransfer -Source https://www.7-zip.org/a/7z1900-x64.exe -Destination 7zip_installer.exe"
     start "" 7zip_installer.exe /S
 
     echo.
-    echo Popular tools successfully installed.
+    echo Popular tools installed successfully.
     pause
     goto menu
 )
