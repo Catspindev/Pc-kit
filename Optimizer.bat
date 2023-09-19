cae89853329a18f0572e2e57ad279ef1b4735439
Catspindev <Catspindev@catspin.eu.org>
2023-09-19 14:36:39 +0200

    Update Optimizer.bat

--- Optimizer.bat
+++ Optimizer.bat
@@ -1,114 +1,54 @@
 batch
 @echo off
-title PC Optimizer
+title Catspins PC Kit
 
-:menu
+:set_language
 cls
-echo PC Optimizer
-echo 1. Clean temporary files
-echo 2. Clear recycle bin
-echo 3. Uninstall unused programs
-echo 4. Defragment hard drive
-echo 5. Exit
+echo Catspins PC Kit
+echo Please select a language:
+echo 1. English-EN
+echo 2. Spanish-ES
+echo 3. Swedish-SV
+set /p lang_selection=Enter language selection (1-3):
 
-set /p choice=Enter your choice (1-5): 
-
-if "%choice%"=="1" (
-    call :clean_temp_files
-    goto menu
-) else if "%choice%"=="2" (
-    call :clear_recycle_bin
-    goto menu
-) else if "%choice%"=="3" (
-    call :uninstall_unused_programs
-    goto menu
-) else if "%choice%"=="4" (
-    call :defragment_hard_drive
-    goto menu
-) else if "%choice%"=="5" (
-    exit
+if "%lang_selection%"=="1" (
+    set "language=English-EN"
+) else if "%lang_selection%"=="2" (
+    set "language=Spanish-ES"
+) else if "%lang_selection%"=="3" (
+    set "language=Swedish-SV"
 ) else (
-    echo Invalid choice!
+    echo Invalid language selection!
     pause
-    goto menu
+    goto :set_language
 )
 
-:clean_temp_files
-echo Cleaning temporary files...
-echo **********************************
-del /q %TEMP%\*.*
-echo Temporary files cleaned successfully.
-pause
-exit
-
-:clear_recycle_bin
-echo Clearing recycle bin...
-echo **********************************
-echo Y|Del %systemdrive%\$Recycle.bin\*.*
-echo Recycle bin cleared successfully.
-pause
-exit
-
-:uninstall_unused_programs
-echo Uninstalling unused programs...
-echo **********************************
-wmic product get name,identifyingNumber > "%TEMP%\installed_programs.txt"
-for /f "skip=1 tokens=*" %%A in ('type "%TEMP%\installed_programs.txt"') do (
-    echo Uninstalling %%A...
-    msiexec /x %%A /qn
-)
-del "%TEMP%\installed_programs.txt"
-echo Unused programs uninstalled successfully.
-pause
-exit
-
-:defragment_hard_drive
-echo Defragmenting hard drive...
-echo **********************************
-defrag /c /h /u /v
-echo Hard drive defragmented successfully.
-pause
-exit
-```
-
-This batch script presents a menu with options to perform different PC optimization tasks. You can customize each option based on your requirements. When a user chooses an option, the corresponding label is called, and the task is executed. After completion, the script returns to the menu. The script can be terminated by selecting option 5.
-
-To add more options or modify existing ones, you can adjust the menu and add additional labels with their respective tasks.
-add befor runing a promt u have to agree with
-
-Sure! Here's the updated version of the PC optimizer batch script with an agreement prompt:
-
-```batch
-@echo off
-title PC Optimizer
-
-setlocal enabledelayedexpansion
-
-:check_language
+:check_agreement
 cls
-echo PC Optimizer
-echo Language: English-EN
-echo.
-set /p agree=Do you agree to proceed? (Y/N): 
+echo Catspins PC Kit (%language%)
+echo Do you agree to proceed? (Y/N):
+set /p agree=
 
 if /i "%agree%"=="Y" (
     goto :menu
-) else (
-    echo You must agree to proceed.
+) else if /i "%agree%"=="N" (
+    exit
+ else (
+)    echo Invalid input! Please enter Y or N.
     pause
-    goto :check_language
+    goto :check_agreement
 )
 
 :menu
 cls
-echo PC Optimizer
+echo Catspins PC Kit (%language%)
 echo 1. Clean temporary files
 echo 2. Clear recycle bin
 echo 3. Uninstall unused programs
 echo 4. Defragment hard drive
 echo 5. Exit
 
-set /p choice=Enter your choice (1-5): 
+set /p choice=Enter your choice (1-5):
 
 if "%choice%"=="1" (
     call :clean_temp_files
@@ -133,7 +73,7 @@ if "%choice%"=="1" (
 :clean_temp_files
 echo Cleaning temporary files...
 echo **********************************
-del /q %TEMP%\*.*
+del /q "%TEMP%\*.*"
 echo Temporary files cleaned successfully.
 pause
 exit
@@ -141,7 +81,7 @@ exit
 :clear_recycle_bin
 echo Clearing recycle bin...
 echo **********************************
-echo Y|Del %systemdrive%\$Recycle.bin\*.*
+echo Y|Del /F /S /Q "%systemdrive%\$Recycle.bin\*.*"
 echo Recycle bin cleared successfully.
 pause
 exit
