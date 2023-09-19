033872c9d85f8e4734858da43a46fea26b717153
Catspindev <Catspindev@catspin.eu.org>
2023-09-19 16:41:15 +0200

    Update Optimizer.bat

--- Optimizer.bat
+++ Optimizer.bat
@@ -51,9 +51,35 @@ if "%choice%"=="4" (
 
 if "%choice%"=="5" (
     echo Installing popular tools...
-    start /wait "" "https://obsproject.com/download"
-    start /wait "" "https://www.7-zip.org/download.html"
-    echo Popular tools installed successfully.
+
+    echo Checking if curl is installed...
+    where curl >nul 2>nul
+    if %errorlevel% neq 0 (
+        echo curl is not installed. Downloading curl installer...
+        curl -o curl_installer.exe https://curl.se/windows/dl-7.78.0_1/curl-7.78.0_1-win64-mingw.zip
+        echo.
+        echo Installing curl...
+        start "" curl_installer.exe
+        echo.
+        echo curl installed successfully.
+    ) else (
+        echo curl is already installed.
+    )
+
+    echo Downloading OBS installer...
+    curl -o obs_installer.exe https://cdn-fastly.obsproject.com/downloads/OBS-Studio-27.0.1-Full-Installer-x64.exe
+    echo.
+    echo Running OBS installer...
+    start "" obs_installer.exe /S
+
+    echo Downloading 7-Zip installer...
+    curl -o 7zip_installer.exe https://www.7-zip.org/a/7z1900-x64.exe
+    echo.
+    echo Running 7-Zip installer...
+    start "" 7zip_installer.exe /S
+
+    echo.
+    echo Popular tools successfully installed.
     pause
     goto menu
 )
